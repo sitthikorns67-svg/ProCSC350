6fcd44bf7ae5c7860a9448ba3e6674e6a36ef529
Nattasith67 <nattasith.k67@rsu.ac.th>
2026-04-23 09:51:00 +0700

    add new structuer add new coment

---
+++ app/course/[id]/quiz/[id]/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Page() {
+
+  /*
+      หลังจากกดปุ่ม quiz จะแสดงแบบทดสอบ
+      api = https://online-cours-api-puap.vercel.app/api/quizzes/[lesson id]
+  */
+  return (
+    <div>Page</div>
+  )
+}
