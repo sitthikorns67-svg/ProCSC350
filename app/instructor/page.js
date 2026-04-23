8f2453694f9f2aa75eba0053b45e4c2f2111afde
Nattasith67 <nattasith.k67@rsu.ac.th>
2026-04-23 09:57:23 +0700

    remove student

--- app/instructor/page.js
+++ app/instructor/page.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <div>
+        <h1>instructor page</h1>
+      </div>
+    </div>
   )
 }
