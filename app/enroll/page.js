3a373e8101e7cd60338247a5a8f4e59859465d80
Nattasith67 <nattasith.k67@rsu.ac.th>
2026-04-23 10:18:05 +0700

    comment enroll

--- app/enroll/page.js
+++ app/enroll/page.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export default function Page() {
+
+  /* 
+    หน้า enroll จะแสดง course ที่เราลงทะเบียนเรียน ท้ังหมด 
+    api = https://online-cours-api-puap.vercel.app/api/enrollments?student_id=[id]
+  */
+
   return (
     <div>page</div>
   )
